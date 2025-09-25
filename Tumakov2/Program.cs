e0685e937366a06efc6df143012aa4772d89c995
Ilgam <ilgamiltinbaev91734@gmail.com>
2025-09-25 11:49:49 +0300

    .

--- Tumakov2/Program.cs
+++ Tumakov2/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+namespace Tumakov2
+{
+    enum Bank
+    {
+        Saving,
+        Current
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            //Упр 3.1
+            Bank now = Bank.Saving;
+            Console.WriteLine(now);
+        }
+    }
+}
